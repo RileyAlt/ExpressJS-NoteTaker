91def693616f4b3254b3f3756386c3145d651b86
RileyAlt <rileyaltenburg@gmail.com>
2023-03-01 16:57:10 -0600

    JavaScript Server

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require ('express')
+const path = require('path')
+
+const app = express()
+const PORT = 3001;
+
+app.use(express.urlencoded({ extended: true })); 
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, './public/index.html')));
+
+require('./api/routing/apiRoutes.js')(app);
+
+require('./api/routing/htmlRoutes.js')(app);
+
+// Listen for connections
+app.listen(PORT, () =>
+  console.info(`Example app listening at http://localhost:${PORT} 🚀`)
+);
 
